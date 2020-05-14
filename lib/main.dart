99bff2f85a8009921270a51e0451ba2f8ef269dc
kharioki <antolisah@gmail.com>
2020-05-14 22:52:49 +0300

    text and rows

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class AvengersCard extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
               'NAME',
@@ -25,6 +26,62 @@ class AvengersCard extends StatelessWidget {
                 color: Colors.grey,
                 letterSpacing: 2.0,
               ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Tony Kharioki',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'CURRENT AVENGER LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Bad-ass',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'kharioki@avengers.com',
+                  style: TextStyle(
+                    color: Colors.amberAccent[100],
+                    letterSpacing: 1.0,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
